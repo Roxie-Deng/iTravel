35e9d811aeedc535222906bccfe2522800f3c72b
Roxie-Deng <y.deng2@universityofgalway.ie>
2024-07-18 15:36:08 +0100

    Updated dependencies, configured JWT, created User/Role models and repositories, set up Spring Security, implemented authentication services and exception handling.

--- src/main/java/com/example/iTravel/model/Role.java
+++ src/main/java/com/example/iTravel/model/Role.java
@@ -4,15 +4,6 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-/**
- * ClassName: Role
- * Package: com.example.iTravel.model
- * Description:
- *
- * @Author Yuki
- * @Create 17/07/2024 15:09
- * @Version 1.0
- */
 @Data
 @Document(collection = "roles")
 public class Role {
