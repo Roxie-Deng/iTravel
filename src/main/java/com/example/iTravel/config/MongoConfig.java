ec158d77bca01402c7252d2c64ad4e054d03f16c
Roxie-Deng <y.deng2@universityofgalway.ie>
2024-08-04 22:29:39 +0100

    Added a global exception handler.

--- src/main/java/com/example/iTravel/config/MongoConfig.java
+++ src/main/java/com/example/iTravel/config/MongoConfig.java
@@ -1,6 +1,8 @@
 package com.example.iTravel.config;
 
+import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -12,16 +14,23 @@ import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
     @Override
     protected String getDatabaseName() {
-        return "test"; // 实际使用的数据库名
+        return "test"; // 从配置文件中读取，也可通过 @Value 注解来动态获取
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri); // 使用配置文件中的 URI 创建 MongoClient
     }
 
     @Bean
     @Override
     public MongoDatabaseFactory mongoDbFactory() {
-        // 注意更新 MongoDB 的 URI
-        return new SimpleMongoClientDatabaseFactory(MongoClients.create(), getDatabaseName());
+        return new SimpleMongoClientDatabaseFactory(mongoClient(), getDatabaseName());
     }
 
     @Bean
