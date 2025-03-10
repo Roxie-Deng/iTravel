f949dc4855333d43829a31fcb87344d0f35b8d12
Roxie-Deng <y.deng2@universityofgalway.ie>
2025-03-10 19:41:25 +0000

    Identify deprecated class and add explanatory comments

--- src/main/java/com/example/iTravel/controller/UserController.java
+++ src/main/java/com/example/iTravel/controller/UserController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Likely useless now
+ * Backend TravelGuideController and POIController
+ */
+
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin(origins = "http://localhost:3000")
