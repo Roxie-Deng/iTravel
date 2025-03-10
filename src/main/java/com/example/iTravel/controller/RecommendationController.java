ca4406eba351abd584b3caa20707070fddf562f1
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2025-03-10 19:44:24 +0000

    Update RecommendationController.java

--- src/main/java/com/example/iTravel/controller/RecommendationController.java
+++ src/main/java/com/example/iTravel/controller/RecommendationController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Likely useless now
+ * Frontend localForage
+ */
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class RecommendationController {
